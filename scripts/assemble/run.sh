f2a4b37ac10693551c3da074ac8d86ea48bea5e3
Bot <dev@verygoodsecurity.com>
2022-04-21 13:34:54 +0000

    Regenerate client from commit 1def794 of spec repo

--- scripts/assemble/run.sh
+++ scripts/assemble/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LIB_VERSION=${LIB_VERSION:-0.0.1.dev$(date "+%Y%m%d%H%M")}
+LIB_VERSION=${LIB_VERSION:-0.0.1.alpha$(date "+%Y%m%d%H%M")}
 
 # fix version
 grep -rl XXX.YYY.ZZZ . | xargs sed -i "s/XXX.YYY.ZZZ/${LIB_VERSION}/g"
